6f12b0b1e988056480b88a8fa78158010c61af0a
alisa <alisal.singyee@gmail.com>
2026-07-15 15:47:22 -0700

    to do list

--- index.ts
+++ index.ts
@@ -19,3 +19,15 @@ app.message(async (event) => {
 });
 
 await app.start();
+
+// to do:
+// join slack huddle when user joins huddle
+// leave slack huddle when user leaves huddle
+// send welcome message when user joins channel
+// play music when users are in huddle
+// track amount of time that users are in huddles
+
+// users can:
+// see their total amount of hours spent in huddles
+// earn coins for the amount of time they spend in huddles
+// spend coins on games to play in huddles --> after spending 30 minutes in a huddle, the bot will ping users
