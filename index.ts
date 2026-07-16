ec14c8edccc9fbdc67cfdd1618bfb1bc24a9abe3
Alisa Leung <alisal.singyee@gmail.com>
2026-07-15 19:56:20 -0700

    computer has no storage so im migrating to another

--- index.ts
+++ index.ts
@@ -10,12 +10,28 @@ const app = new App({
     logLevel: LogLevel.DEBUG,
 });
 
+// when alisa sends a message
 app.message(async (event) => {
     console.log("message event");
     console.dir(event.payload, { depth: null });
     if (event.payload.subtype) return;
     if (event.payload.user !== 'U08STAQPGUR') return;
     await event.say("wow what a cool message");
+});
+
+// when someone joins the channel
+app.event('member_joined_channel', async ({ event, client, logger }) => {
+    try {
+        const userId = event.user;
+        const channelId = event.channel;
+        if (channelId !== 'C0BHGKC7P51') return;
+        await client.chat.postMessage({
+            channel: channelId,
+            text: `welcome to the channel, <@${userId}>!`,
+        });
+    } catch (error) {
+        logger.error('error handling member_joined_channel event: ', error);
+    }
 });
 
 await app.start();
@@ -31,3 +47,11 @@ await app.start();
 // see their total amount of hours spent in huddles
 // earn coins for the amount of time they spend in huddles
 // spend coins on games to play in huddles --> after spending 30 minutes in a huddle, the bot will ping users
+
+// running a game through a canvas
+// searching messages
+// pinning messages
+// uploading and messing around with files
+// messing around with custom emojis --> apples to apples but with custom slack emojis
+// changing statuses?
+// dialogs? --> they act as modals where you can input information
